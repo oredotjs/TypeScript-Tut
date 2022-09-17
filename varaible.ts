aa1ae48ccdd9ad750b1c052ca75b5229d9e45047
Emmanuel Oreoluwa <awuloero13@gmail.com>
2022-09-17 20:21:29 +0100

    :tada: initial commit

---
+++ varaible.ts
@@ -0,0 +1,18 @@
+// Types
+// number string boolean any
+
+let myCrib: string = "John";
+myCrib = "John";
+let draac: number = 1
+console.log(draac)
+
+
+
+// type Inference
+let tvshow = 123;
+tvshow = false;
+
+
+const leaf = (ore) => {
+    console.log(ore)
+}
