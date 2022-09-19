bdcc249f08483c82cd34396302595088b912de4d
Emmanuel Oreoluwa <awuloero13@gmail.com>
2022-09-19 15:07:12 +0100

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+const norma = {
+    breed: "Silie",
+    eggdPeerWeek: 23,
+    name: "norma",
+    age: 234
+};
---
+++ index.ts
@@ -0,0 +1,16 @@
+interface Chicken {
+    breed: String;
+    eggdPeerWeek: number;
+    name: String;
+}
+interface Chicken {
+   age: number
+}
+
+const norma: Chicken = {
+    breed: "Silie",
+    eggdPeerWeek: 23,
+    name: "norma",
+    age: 234
+
+}
